b0f84f1304eb1b91688150ae501773bb8f8bdfee
bobweein <91543576+bobweein@users.noreply.github.com>
2021-12-12 22:34:19 +0800

    Create name.h

---
+++ WEEK3/name.h
@@ -0,0 +1,16 @@
+#ifndef _NAME_H
+namespace SALES
+{
+	const int QUARTES = 4;
+	struct Sales
+	{
+		double sales[QUARTES];
+		double average;
+		double max;
+		double min;
+	};
+	void setSales(Sales& s, const double ar[], int n);
+	void setSales(Sales& s);
+	void showSales(const Sales& s);
+}
+#endif 
